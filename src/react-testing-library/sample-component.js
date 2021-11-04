a95c5666a134f45698cc7b93a368b8cef6bdac25
Joe <joimee.cajandab@gmail.com>
2021-11-04 19:16:39 +0800

    ✨ react-testing-library

--- src/react-testing-library/sample-component.js
+++ src/react-testing-library/sample-component.js
@@ -1,1 +1,43 @@
-export const helloWorld = () => 'Hello world';
+import React, { useState, useEffect } from "react";
+
+export const Modal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  return (
+    <div>
+      <button data-testid="openModalBtn" onClick={handleOpenModal}>
+        Open Modal
+      </button>
+
+      <div
+        className={`modal ${isModalOpen ? "modal--active" : ""}`}
+        data-testid="modal"
+      >
+        <p>Lorem ipsum dolor sit amet</p>
+      </div>
+    </div>
+  );
+};
+
+export const Loader = ({ component: Component }) => {
+  const [isLoaded, setIsLoaded] = React.useState(false);
+
+  useEffect(() => {
+    // imagine we're fetching...
+    setIsLoaded(true);
+  }, []);
+
+  return isLoaded ? <Component /> : <p>Loading...</p>;
+};
+
+const HelloWorld = () => (
+  <div>
+    <p data-testid="helloWorld">Hello World</p>
+  </div>
+);
+
+export default HelloWorld;
--- src/react-testing-library/sample-component.spec.js
+++ src/react-testing-library/sample-component.spec.js
@@ -1,5 +1,43 @@
-import { helloWorld } from './sample-component';
+import React from "react";
+import HelloWorld, { Modal, Loader } from "./sample-component";
+import { render, screen, fireEvent } from "@testing-library/react";
 
-it('should test hello world', () => {
-  expect(helloWorld()).toMatch('Hello world');
+describe("./sample-component.js", () => {
+  test("renders <HelloWorld />", () => {
+    render(<HelloWorld />);
+
+    const helloWorldText = screen.getByTestId("helloWorld");
+
+    expect(helloWorldText).toBeInTheDocument();
+  });
+
+  test("checks if the model is open", () => {
+    render(<Modal />);
+
+    const openModalBtn = screen.getByTestId("openModalBtn");
+    const modal = screen.getByTestId("modal");
+
+    // modal is hidden
+    expect(modal).not.toHaveClass("modal--active");
+
+    // fire the event here
+    fireEvent.click(openModalBtn);
+
+    // modal is visible
+    expect(modal).toHaveClass("modal--active");
+  });
+
+  test("renders loader component", () => {
+    const setIsLoaded = jest.fn();
+
+    // this only works because we use React.useState inside our component
+    jest
+      .spyOn(React, "useState")
+      .mockImplementation(() => [false, setIsLoaded]);
+
+    render(<Loader component={HelloWorld} />);
+
+    expect(setIsLoaded).toHaveBeenCalledTimes(1);
+    expect(setIsLoaded).toHaveBeenCalledWith(true);
+  });
 });
